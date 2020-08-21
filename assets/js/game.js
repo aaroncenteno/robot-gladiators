928ea97c856b48e4fc3b89be34a32098ebec026d
ASC8 <asamcent@gmail.com>
2020-08-21 12:50:54 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -121,6 +121,55 @@ var endGame = function() {
     }
   };
 
+  var shop = function () {
+    var shopOptionPrompt = window.prompt(
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL, 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    // use switch to carry out action 
+    switch (shopOptionPrompt) {
+      case "REFILL": //new case
+      case "refill": 
+        if (playerMoney >= 7){
+          window.alert("Refilling player's health by 20 for 7 dollars.");
+
+          // increase health and decrease money
+          playerHealth = playerHealth + 20;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+
+        break;
+      case "UPGRADE":
+      case "upgrade":
+        if (playerMoney >= 7) {
+          window.alert("Upgrading  player's attack by 6 for 7 dollars.");
+
+          // increase attack and decrease money
+          playerAttack = playerAttack + 6;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+
+        break;
+      case "Leave": 
+      case "leave":
+        window.alert("Leaving the store.");
+
+        // do nothing, so function will end
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+
+      //  call sho() again to force player to pick a valid option
+      shop();
+      break;
+    }
+  };
+
 // for (var i = 0; i < enemyNames.length; i++) {
 //     var pickedEnemyNames = enemyNames[i];
 //     enemyHealth = 50;
