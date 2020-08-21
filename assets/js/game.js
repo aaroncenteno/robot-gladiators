eea2600a3bffd54b913f5c2cee4b60e789095fa4
ASC8 <asamcent@gmail.com>
2020-08-21 13:45:26 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,7 +22,7 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
@@ -30,7 +30,9 @@ var fight = function(enemyName) {
 
     else if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + " attacked "  + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -50,7 +52,9 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -80,7 +84,7 @@ var startGame = function() {
     
             var pickedEnemyName = enemyNames[i];
             
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
     
             fight(pickedEnemyName);
 
@@ -168,6 +172,12 @@ var endGame = function() {
       shop();
       break;
     }
+  };
+
+  var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
   };
 
 // for (var i = 0; i < enemyNames.length; i++) {
